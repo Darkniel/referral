7f1777ae9b6a4e917c49ec462c324bf51481b94a
Darkniel <daniel.visualfx@gmail.com>
2018-12-18 18:15:10 -0500

    Added Method to extract referer

--- src/android/attribution.java
+++ src/android/attribution.java
@@ -34,22 +34,24 @@ public class attribution extends BroadcastReceiver {
         edit.apply();
 		
     }
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("getReferer")) {
+            String message = args.getString(0);
+            this.getReferer(message, callbackContext);
+            return true;
+        }
+        return false;
+    }
 
-    // @Override
-    // public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    //     if (action.equals("coolMethod")) {
-    //         String message = args.getString(0);
-    //         this.coolMethod(message, callbackContext);
-    //         return true;
-    //     }
-    //     return false;
-    // }
-
-    // private void coolMethod(String message, CallbackContext callbackContext) {
-    //     if (message != null && message.length() > 0) {
-    //         callbackContext.success(message);
-    //     } else {
-    //         callbackContext.error("Expected one non-empty string argument.");
-    //     }
+    private void getReferer(String message, CallbackContext callbackContext) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String referer = preferences.getString("referrer", "");
+        if(!referer.equalsIgnoreCase(""))
+        {
+            callbackContext.success(referer);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
     }
 }
--- www/attribution.js
+++ www/attribution.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'attribution', 'coolMethod', [arg0]);
+exports.getReferer = function (arg0, success, error) {
+    exec(success, error, 'attribution', 'getReferer', [arg0]);
 };
